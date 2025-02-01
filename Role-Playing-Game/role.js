5a515b23aac41ac461a9a990ab67de56b09e7bc3
Khin <khinthirikyaw8@gmail.com>
2025-02-01 13:16:06 +0700

    implemented some fight fucntion, buy function,game method.

--- Role-Playing-Game/role.js
+++ Role-Playing-Game/role.js
@@ -167,7 +167,18 @@ function fightDragon() {
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealth.innerText = monsterHealth;
 }
 
-function attack() {}
+function attack() {
+  text.innerText = `The ${monsters[fighting].name} attacks.`;
+  text.innerText += ` You attack it with your ${weapons[currentWeaponIndex].name}.`;
+  health -= monsters[fighting].level;
+  monsterHealth -=
+    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+}
 function dodge() {}
